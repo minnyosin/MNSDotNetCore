92c8447ddf572a2597e4b4c16ea2344c40b5183e
Min Nyo Sin <minnyosin.apple@gmail.com>
2025-03-26 08:42:14 +0630

    1st push

---
+++ MNSDotNetCore.ConsoleApp/AdoDotNetExample.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace MNSDotNetCore.ConsoleApp
+{
+    internal class AdoDotNetExample
+    {
+        private readonly SqlConnectionStringBuilder _sqlConnectionStringBuilder = new SqlConnectionStringBuilder()
+        {
+           DataSource = ".",
+           InitialCatalog = "DotNetTraining",
+           UserID = "sa",
+           Password = "sa@123",
+        };
+        public void Read()
+        {
+            SqlConnection connection = new SqlConnection(_sqlConnectionStringBuilder.ConnectionString);
+
+            connection.Open();
+            Console.WriteLine("Connection open!");
+
+            string query = "select * from Tbl_blog";
+            SqlCommand cmd = new SqlCommand(query, connection);
+            SqlDataAdapter sqlDataAdaptor = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            sqlDataAdaptor.Fill(dt);
+
+            connection.Close();
+            Console.WriteLine("Connection close!");
+
+            foreach (DataRow dr in dt.Rows)
+            {
+                Console.WriteLine("Blog Id => " + dr["BlogId"]);
+                Console.WriteLine("Blog Title => " + dr["BlogTitle"]);
+                Console.WriteLine("Blog Author => " + dr["BlogAuthor"]);
+                Console.WriteLine("Blog Content => " + dr["BlogContent"]);
+                Console.WriteLine("--------------------------------------");
+            }
+        }
+
+        public void Create(string title, string author, string content)
+        {
+            SqlConnection connection = new SqlConnection(_sqlConnectionStringBuilder.ConnectionString);
+
+            connection.Open();
+
+            string query = @"INSERT INTO [dbo].[Tbl_Blog]
+           ([BlogTitle]
+           ,[BlogAuthor]
+           ,[BlogContent])
+     VALUES
+           (@BlogTitle,
+			@BlogAuthor,
+			@BlogContent)";
+            SqlCommand cmd = new SqlCommand(query, connection);
+            cmd.Parameters.AddWithValue("@BlogTitle", title);
+            cmd.Parameters.AddWithValue("@BlogAuthor", author);
+            cmd.Parameters.AddWithValue("@BlogContent", content);
+            int result = cmd.ExecuteNonQuery();
+
+            connection.Close();
+
+            string message = result > 0 ? "Saving Successful!" : "Saving Failed!";
+            Console.WriteLine(message);
+
+            
+            
+        }
+    }
+}
--- MNSDotNetCore.ConsoleApp/Program.cs
+++ MNSDotNetCore.ConsoleApp/Program.cs
@@ -1,3 +1,49 @@
-﻿
+﻿using System.Data.SqlClient;
+using System.Data;
+using MNSDotNetCore.ConsoleApp;
+
 Console.WriteLine("Hello, World!");
+
+//SqlConnectionStringBuilder stringBuilder = new SqlConnectionStringBuilder();
+//stringBuilder.DataSource = ".";
+//stringBuilder.InitialCatalog = "DotNetTraining";
+//stringBuilder.UserID = "sa";
+//stringBuilder.Password = "sa@123";
+//SqlConnection connection = new SqlConnection(stringBuilder.ConnectionString);
+//connection.Open();
+//Console.WriteLine("Connection open!");
+
+//string query = "select * from Tbl_blog";
+//SqlCommand cmd = new SqlCommand(query, connection);
+//SqlDataAdapter sqlDataAdaptor = new SqlDataAdapter(cmd);
+//DataTable dt = new DataTable();
+//sqlDataAdaptor.Fill(dt);
+
+
+//connection.Close();
+//Console.WriteLine("Connection close!");
+
+//// dataset => datatable
+//// datatable => datarow
+//// datarow => datacolumn
+
+//foreach (DataRow dr in dt.Rows)
+//{
+//    Console.WriteLine("Blog Id => " + dr["BlogId"]);
+//    Console.WriteLine("Blog Title => " + dr["BlogTitle"]);
+//    Console.WriteLine("Blog Author => " + dr["BlogAuthor"]);
+//    Console.WriteLine("Blog Content => " + dr["BlogContent"]);
+//    Console.WriteLine("--------------------------------------");
+//    Console.ReadKey();
+//}
+
+// Ado.Net Read
+//CRUD
+
+
+AdoDotNetExample adoDotNetExample = new AdoDotNetExample();
+//adoDotNetExample.Read();
+adoDotNetExample.Create("title", "author", "content");
+
+Console.ReadKey();
 
